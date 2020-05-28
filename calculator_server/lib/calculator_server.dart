13043b5d97839a15c71a5a74b875a94d3d3bda0f
repin <mr.malarg@gmail.com>
2020-05-28 10:19:34 +0500

    init

---
+++ calculator_server/lib/calculator_server.dart
@@ -0,0 +1,11 @@
+/// calculator_server
+///
+/// A Aqueduct web server.
+library calculator_server;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:aqueduct/aqueduct.dart';
+
+export 'channel.dart';
